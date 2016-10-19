ccf34bd6087520dd000f166221df11540a973da9
dikrandono <dikrandono@gmail.com>
2016-10-19 10:50:38 +0200

    Jms Liberary version bug

---
+++ src/main/java/com/finalist/model/config/SpringSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.finalist.model.config;
+
+ 
+public class SpringSecurityConfig {
+
+	public SpringSecurityConfig() {
+	}
+	
+	/*
+	  @Autowired
+	  public void configureGlobal(AuthenticationManagerBuilder auth) {
+	   
+		  try {
+			auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	  }
+	*/
+
+}
